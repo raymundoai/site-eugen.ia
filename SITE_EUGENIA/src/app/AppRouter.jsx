f54d22308d17f503edcdbdb93cf66c50d80c32c4
Raymundo <aeraartificial@gmail.com>
2026-05-14 14:30:39 -0300

    feat: monta ParticleField na homepage via AppRouter

--- SITE_EUGENIA/src/app/AppRouter.jsx
+++ SITE_EUGENIA/src/app/AppRouter.jsx
@@ -1,25 +1,52 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { PageShell } from '../templates/PageShell'
-import { HomePage } from '../pages/HomePage'
+import { useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
+import { BackgroundField } from '../components/BackgroundField'
+import { GlobalCursor } from '../components/GlobalCursor'
+import { useLenis } from '../hooks/useLenis'
+import { useScrollReveal } from '../hooks/useScrollReveal'
+import { ParticleField } from '../components/ParticleField'
 import { AboutPage } from '../pages/AboutPage'
-import { FaqPage } from '../pages/FaqPage'
 import { ContactPage } from '../pages/ContactPage'
-import { ThanksPage } from '../pages/ThanksPage'
+import { FaqPage } from '../pages/FaqPage'
+import { HomePage } from '../pages/HomePage'
 import { NotFoundPage } from '../pages/NotFoundPage'
+import { ServicesPage } from '../pages/ServicesPage'
+import { ThanksPage } from '../pages/ThanksPage'
 
 export function AppRouter() {
+  const location = useLocation()
+  useLenis()
+  useScrollReveal()
+
+  useEffect(() => {
+    if (!location.hash) return
+    const id = location.hash.slice(1)
+    const el = document.getElementById(id)
+    if (!el) return
+    const top = el.getBoundingClientRect().top + window.scrollY
+    window.scrollTo({ top, behavior: 'smooth' })
+  }, [location.hash, location.pathname])
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<PageShell />}>
+    <>
+      <BackgroundField />
+      {location.pathname === '/' && <ParticleField />}
+      <GlobalCursor />
+      <Header />
+      <main>
+        <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/servicos" element={<ServicesPage />} />
           <Route path="/sobre" element={<AboutPage />} />
           <Route path="/faq" element={<FaqPage />} />
           <Route path="/contato" element={<ContactPage />} />
           <Route path="/obrigado" element={<ThanksPage />} />
           <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </main>
+      <Footer />
+    </>
   )
 }
